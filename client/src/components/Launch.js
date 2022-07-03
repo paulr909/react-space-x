518c85febaec11eb1138e84c17511a1b3e6f343d
paulrogers <paul303808909@protonmail.com>
2022-07-03 11:30:59 +0100

    optimise imports

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import classNames from "classnames";
 import Spinner from "./Spinner";
 
